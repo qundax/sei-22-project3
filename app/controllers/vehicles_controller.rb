10604576b7af6931358ad2be650632f21012447b
osbornechan <osbornecab@gmail.com>
2020-05-18 20:34:01 +0800

    Added CRUD for vehicles

---
+++ app/controllers/vehicles_controller.rb
@@ -0,0 +1,51 @@
+class VehiclesController < ApplicationController
+  #before_action :authenticate_user!, :except => [ :index ]
+
+  def index
+    @vehicles = Vehicle.all
+  end
+
+  def new
+  end
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+    #@vehicle.user = current_user
+
+    if @vehicle.save
+      redirect_to @vehicle
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  def edit
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  def update
+    @vehicle = Vehicle.find(params[:id])
+
+    if @vehicle.update(vehicle_params)
+      redirect_to vehicle_path(@vehicle)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @vehicle = Vehicle.find(params[:id])
+    @vehicle.destroy
+
+    redirect_to vehicles_path
+  end
+
+  private
+    def vehicle_params
+      params.require(:vehicle).permit(:license_plate, :vehicle_model)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+vehicle1 = Vehicle.create!(license_plate:"SFX1386H", vehicle_model:"Honda")
+vehicle2 = Vehicle.create!(license_plate:"SJG6768Z", vehicle_model:"Toyota")
+vehicle3 = Vehicle.create!(license_plate:"SGP2756Y", vehicle_model:"Mercedes")
+
+#vehicle1 = Vehicle.create!(user_id:1,license_plate:"SFX1386H", vehicle_model:"Honda")
+#vehicle2 = Vehicle.create!(user_id:2, license_plate:"SJG6768Z", vehicle_model:"Toyota")
+#vehicle3 = Vehicle.create!(user_id:3,license_plate:"SGP2756Y", vehicle_model:"Mercedes")
