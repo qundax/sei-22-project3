f242e3d4a5bc345373896b5262fc26b70b4a9e3a
kenneththesheep <kenneththesheep@gmail.com>
2020-05-21 16:33:54 +0800

    reset schema

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -1,6 +1,6 @@
 class Lot < ActiveRecord::Base
-  #belongs_to :carpark
-  #belongs_to :vehicle, optional: true
+  belongs_to :carpark
+  belongs_to :vehicle, optional: true
 
   validates :lotNumber, presence: true, uniqueness: { case_sensitive: false }
 end
