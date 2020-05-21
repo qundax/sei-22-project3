5213ee200ab403310fc6c3842dfebccaf96c22a6
kenneththesheep <kenneththesheep@gmail.com>
2020-05-21 21:11:25 +0800

    edited the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -343,8 +343,8 @@
 )
 
 
-ascencio = Carpark.create(
-  :location => 'Ascencio', :totallots => 10, :photo_url=> 'http://i.huffpost.com/gen/1952378/images/o-WEIRD-AL-facebook.jpg', :region_id=>4
+ascencio = seletar1.carparks.create(
+  :location => 'Ascencio', :totallots => 10, :photo_url=> 'http://i.huffpost.com/gen/1952378/images/o-WEIRD-AL-facebook.jpg'
 )
 
     ascencio.lots.create({lotNumber: 'A1'})
@@ -358,8 +358,8 @@ ascencio = Carpark.create(
     ascencio.lots.create({lotNumber: 'A9'})
     ascencio.lots.create({lotNumber: 'A10'})
 
-regency = Carpark.create(
-  :location => 'Regency', :totallots => 10, :photo_url=> 'http://www.gannett-cdn.com/-mm-/b0ad212381eab60e31d1f067f1c478cea741469a/c=0-10-3443-1963&r=x1683&c=3200x1680/local/-/media/USATODAY/GenericImages/2014/03/31//1396298223000-KISS-KISS-BAND-JY-0718-62187918.jpg', :region_id=>5
+regency = orchard.carparks.create(
+  :location => 'Regency', :totallots => 10, :photo_url=> 'http://www.gannett-cdn.com/-mm-/b0ad212381eab60e31d1f067f1c478cea741469a/c=0-10-3443-1963&r=x1683&c=3200x1680/local/-/media/USATODAY/GenericImages/2014/03/31//1396298223000-KISS-KISS-BAND-JY-0718-62187918.jpg'
 )
     regency.lots.create({lotNumber: 'R1'})
     regency.lots.create({lotNumber: 'R2'})
@@ -372,8 +372,8 @@ regency = Carpark.create(
     regency.lots.create({lotNumber: 'R9'})
     regency.lots.create({lotNumber: 'R10'})
 
-kovan = Carpark.create(
-  :location => 'Kovan Market', :totallots => 10, :photo_url=> 'http://www.gannett-cdn.com/-mm-/b0ad212381eab60e31d1f067f1c478cea741469a/c=0-10-3443-1963&r=x1683&c=3200x1680/local/-/media/USATODAY/GenericImages/2014/03/31//1396298223000-KISS-KISS-BAND-JY-0718-62187918.jpg', :region_id=>8
+kovan = kovan.carparks.create(
+  :location => 'Kovan Market', :totallots => 10, :photo_url=> 'http://www.gannett-cdn.com/-mm-/b0ad212381eab60e31d1f067f1c478cea741469a/c=0-10-3443-1963&r=x1683&c=3200x1680/local/-/media/USATODAY/GenericImages/2014/03/31//1396298223000-KISS-KISS-BAND-JY-0718-62187918.jpg'
 )
 
     kovan.lots.create({lotNumber: 'K1'})
@@ -387,8 +387,8 @@ kovan = Carpark.create(
     kovan.lots.create({lotNumber: 'K9'})
     kovan.lots.create({lotNumber: 'K10'})
 
-jurongpoint = Carpark.create(
-  :location => 'Jurong Point', :totallots => 10, :photo_url=> 'http://www.gannett-cdn.com/-mm-/b0ad212381eab60e31d1f067f1c478cea741469a/c=0-10-3443-1963&r=x1683&c=3200x1680/local/-/media/USATODAY/GenericImages/2014/03/31//1396298223000-KISS-KISS-BAND-JY-0718-62187918.jpg', :region_id=>10
+jurongpoint = jurong1.carparks.create(
+  :location => 'Jurong Point', :totallots => 10, :photo_url=> 'http://www.gannett-cdn.com/-mm-/b0ad212381eab60e31d1f067f1c478cea741469a/c=0-10-3443-1963&r=x1683&c=3200x1680/local/-/media/USATODAY/GenericImages/2014/03/31//1396298223000-KISS-KISS-BAND-JY-0718-62187918.jpg'
 )
 
     jurongpoint.lots.create({lotNumber: 'JP1'})
