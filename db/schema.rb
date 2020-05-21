5323158b697473835d0f64246039dc1dd6084e41
qundax <qunda.lim@outlook.sg>
2020-05-21 09:13:11 +0800

    Added Summon role to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_134539) do
+ActiveRecord::Schema.define(version: 2020_05_21_010625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  
-  create_table "vehicles", force: :cascade do |t|
-    t.string "license_plate"
-    t.string "vehicle_model"
-  end
-  
+
   create_table "carparks", force: :cascade do |t|
     t.string "location"
     t.integer "totallots"
@@ -38,5 +33,27 @@ ActiveRecord::Schema.define(version: 2020_05_18_134539) do
     t.index ["carpark_id"], name: "index_lots_on_carpark_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.boolean "summon", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "license_plate"
+    t.string "vehicle_model"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_vehicles_on_user_id"
+  end
+
   add_foreign_key "lots", "carparks"
+  add_foreign_key "vehicles", "users"
 end
