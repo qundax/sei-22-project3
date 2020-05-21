371e10fe0d8f490c3e49bc50185ea483f2871af4
kenneththesheep <kenneththesheep@gmail.com>
2020-05-21 13:00:08 +0800

    fixed some bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,42 +11,7 @@
 # vehicle2a = Vehicle.create!(user_id:2, license_plate:"SJG6768Z", vehicle_model:"Toyota")
 # vehicle2b = Vehicle.create!(user_id:2,license_plate:"SGP2756Y", vehicle_model:"Mercedes")
 
-# ascencio = Carpark.create(
-#   :location => 'Ascencio', :totallots => 10, :photo_url=> 'http://i.huffpost.com/gen/1952378/images/o-WEIRD-AL-facebook.jpg'
-# )
 
-
-# regency = Carpark.create(
-#   :location => 'Regency', :totallots => 10, :photo_url=> 'http://www.gannett-cdn.com/-mm-/b0ad212381eab60e31d1f067f1c478cea741469a/c=0-10-3443-1963&r=x1683&c=3200x1680/local/-/media/USATODAY/GenericImages/2014/03/31//1396298223000-KISS-KISS-BAND-JY-0718-62187918.jpg'
-# )
-
-
-# kovan = Carpark.create(
-#   :location => 'Kovan Market', :totallots => 10, :photo_url=> 'http://www.gannett-cdn.com/-mm-/b0ad212381eab60e31d1f067f1c478cea741469a/c=0-10-3443-1963&r=x1683&c=3200x1680/local/-/media/USATODAY/GenericImages/2014/03/31//1396298223000-KISS-KISS-BAND-JY-0718-62187918.jpg'
-# )
-
-
-# jurongpoint = Carpark.create(
-#   :location => 'Jurong Point', :totallots => 10, :photo_url=> 'http://www.gannett-cdn.com/-mm-/b0ad212381eab60e31d1f067f1c478cea741469a/c=0-10-3443-1963&r=x1683&c=3200x1680/local/-/media/USATODAY/GenericImages/2014/03/31//1396298223000-KISS-KISS-BAND-JY-0718-62187918.jpg'
-# )
-
-
-
-
-# carparks = Carpark.all
-# carparks.each do |carpark|
-
-#     carpark.lot.create({lotNumber: 'R1'})
-#     carpark.lot.create({lotNumber: 'R2'})
-#     carpark.lot.create({lotNumber: 'R3'})
-#     carpark.lot.create({lotNumber: 'R4'})
-#     carpark.lot.create({lotNumber: 'R5'})
-#     carpark.lot.create({lotNumber: 'R6'})
-#     carpark.lot.create({lotNumber: 'R7'})
-#     carpark.lot.create({lotNumber: 'R8'})
-#     carpark.lot.create({lotNumber: 'R9'})
-#     carpark.lot.create({lotNumber: 'R10'})
-# end
 
 
 #  rafflesplace = Region.create(
@@ -376,3 +341,42 @@
 #   seletar2 = Region.create(
 #    :regionname => 'Seletar 2', :postalcode => '80'
 # )
+
+
+# ascencio = Carpark.create(
+#   :location => 'Ascencio', :totallots => 10, :photo_url=> 'http://i.huffpost.com/gen/1952378/images/o-WEIRD-AL-facebook.jpg', :region_id=>71
+# )
+
+
+# regency = Carpark.create(
+#   :location => 'Regency', :totallots => 10, :photo_url=> 'http://www.gannett-cdn.com/-mm-/b0ad212381eab60e31d1f067f1c478cea741469a/c=0-10-3443-1963&r=x1683&c=3200x1680/local/-/media/USATODAY/GenericImages/2014/03/31//1396298223000-KISS-KISS-BAND-JY-0718-62187918.jpg', :region_id=>80
+# )
+
+
+# kovan = Carpark.create(
+#   :location => 'Kovan Market', :totallots => 10, :photo_url=> 'http://www.gannett-cdn.com/-mm-/b0ad212381eab60e31d1f067f1c478cea741469a/c=0-10-3443-1963&r=x1683&c=3200x1680/local/-/media/USATODAY/GenericImages/2014/03/31//1396298223000-KISS-KISS-BAND-JY-0718-62187918.jpg', :region_id=>113
+# )
+
+
+# jurongpoint = Carpark.create(
+#   :location => 'Jurong Point', :totallots => 10, :photo_url=> 'http://www.gannett-cdn.com/-mm-/b0ad212381eab60e31d1f067f1c478cea741469a/c=0-10-3443-1963&r=x1683&c=3200x1680/local/-/media/USATODAY/GenericImages/2014/03/31//1396298223000-KISS-KISS-BAND-JY-0718-62187918.jpg', :region_id=>120
+# )
+
+
+
+
+carparks = Carpark.all
+
+carparks.each do |carpark|
+    puts "test"
+    carpark.lots.create({lotNumber: 'R1'})
+    carpark.lots.create({lotNumber: 'R2'})
+    carpark.lots.create({lotNumber: 'R3'})
+    carpark.lots.create({lotNumber: 'R4'})
+    carpark.lots.create({lotNumber: 'R5'})
+    carpark.lots.create({lotNumber: 'R6'})
+    carpark.lots.create({lotNumber: 'R7'})
+    carpark.lots.create({lotNumber: 'R8'})
+    carpark.lots.create({lotNumber: 'R9'})
+    carpark.lots.create({lotNumber: 'R10'})
+end
