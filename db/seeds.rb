d6f51f3a9f65e24a4caf6b9baf3c4efa20c7a089
kenneththesheep <kenneththesheep@gmail.com>
2020-05-21 15:19:22 +0800

    editted the seed to test herouku

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,333 +14,333 @@
 
 
 
-#  rafflesplace = Region.create(
-#    :regionname => 'Raffles Place', :postalcode => '01'
-# )
-
-#  cecil = Region.create(
-#    :regionname => 'Cecil', :postalcode => '02'
-# )
-
-#   marina1 = Region.create(
-#    :regionname => 'Marina 1', :postalcode => '03'
-# )
-#   marina2 = Region.create(
-#    :regionname => 'Marina 2', :postalcode => '04'
-# )
-
-#   peoplepark1 = Region.create(
-#    :regionname => 'People Park 1', :postalcode => '05'
-# )
-#   peoplepark2 = Region.create(
-#    :regionname => 'People Park 2', :postalcode => '06'
-# )
-
-#   anson = Region.create(
-#    :regionname => 'Anson', :postalcode => '07'
-# )
-#   tanjongPagar = Region.create(
-#    :regionname => 'Tanjong Pagar', :postalcode => '08'
-# )
-
-#   anson = Region.create(
-#    :regionname => 'Anson', :postalcode => '07'
-# )
-#   tanjongPagar = Region.create(
-#    :regionname => 'Tanjong Pagar', :postalcode => '08'
-# )
-#   bukitmerah = Region.create(
-#    :regionname => 'Bukit Merah', :postalcode => '14'
-# )
-#   queenstown = Region.create(
-#    :regionname => 'Queenstown', :postalcode => '15'
-# )
-
-#   tiongBahru = Region.create(
-#    :regionname => 'Tiong Bahru', :postalcode => '16'
-# )
-
-#   telokBlangah = Region.create(
-#    :regionname => 'Telok Blangah', :postalcode => '09'
-# )
-
-#   harbourfront = Region.create(
-#    :regionname => 'Harbourfront', :postalcode => '10'
-# )
-
-#   pasirPanjang = Region.create(
-#    :regionname => 'Pasir Panjang', :postalcode => '11'
-# )
-
-#   hongLeong = Region.create(
-#    :regionname => 'Hong Leong Garden', :postalcode => '12'
-# )
-
-#   clementiNewTown = Region.create(
-#    :regionname => 'Clementi New Town', :postalcode => '13'
-# )
-
-#   highStreet = Region.create(
-#    :regionname => 'High Street', :postalcode => '17'
-# )
-
-#   middleRoad = Region.create(
-#    :regionname => 'Middle Road', :postalcode => '18'
-# )
-
-#   goldenMile = Region.create(
-#    :regionname => 'Golden Mile', :postalcode => '19'
-# )
-
-#   littleIndia = Region.create(
-#    :regionname => 'Little India', :postalcode => '20'
-# )
-
-#   lavender = Region.create(
-#    :regionname => 'lavender', :postalcode => '21'
-# )
-
-#   orchard = Region.create(
-#    :regionname => 'Orchard', :postalcode => '22'
-# )
-
-#   riverValley = Region.create(
-#    :regionname => 'River Valley', :postalcode => '23'
-# )
-
-#   ardmore = Region.create(
-#    :regionname => 'Ardmore', :postalcode => '24'
-# )
-
-#   bukitTimah = Region.create(
-#    :regionname => 'Bukit Timah', :postalcode => '25'
-# )
-
-#   hollandRoad = Region.create(
-#    :regionname => 'Holland Road', :postalcode => '26'
-# )
-
-#   tanglin = Region.create(
-#    :regionname => 'Tanglin', :postalcode => '27'
-# )
-
-#   wattenEstate = Region.create(
-#    :regionname => 'Watten Estate', :postalcode => '28'
-# )
-
-#   novena = Region.create(
-#    :regionname => 'Novena', :postalcode => '29'
-# )
-
-#   thomson = Region.create(
-#    :regionname => 'Thomson', :postalcode => '30'
-# )
-
-#   balestier = Region.create(
-#    :regionname => 'Balestier', :postalcode => '31'
-# )
-
-#   toaPayoh = Region.create(
-#    :regionname => 'Toa Payoh', :postalcode => '32'
-# )
-
-#   serangoon = Region.create(
-#    :regionname => 'Serangoon', :postalcode => '33'
-# )
-
-#   macpherson1 = Region.create(
-#    :regionname => 'Macpherson 1', :postalcode => '34'
-# )
-
-#   macpherson2 = Region.create(
-#    :regionname => 'Macpherson 2', :postalcode => '35'
-# )
-#   bradell1 = Region.create(
-#    :regionname => 'Bradell 1', :postalcode => '36'
-# )
-
-#     bradell2 = Region.create(
-#    :regionname => 'Bradell 2', :postalcode => '37'
-# )
-
-#   geylang1 = Region.create(
-#    :regionname => 'Geylang 1', :postalcode => '38'
-# )
-
-#   geylang2 = Region.create(
-#    :regionname => 'Geylang 2', :postalcode => '39'
-# )
-#   eunos1 = Region.create(
-#    :regionname => 'Eunos 1', :postalcode => '40'
-# )
-
-#   eunos2 = Region.create(
-#    :regionname => 'Eunos 2', :postalcode => '41'
-# )
+ rafflesplace = Region.create(
+   :regionname => 'Raffles Place', :postalcode => '01'
+)
+
+ cecil = Region.create(
+   :regionname => 'Cecil', :postalcode => '02'
+)
+
+  marina1 = Region.create(
+   :regionname => 'Marina 1', :postalcode => '03'
+)
+  marina2 = Region.create(
+   :regionname => 'Marina 2', :postalcode => '04'
+)
+
+  peoplepark1 = Region.create(
+   :regionname => 'People Park 1', :postalcode => '05'
+)
+  peoplepark2 = Region.create(
+   :regionname => 'People Park 2', :postalcode => '06'
+)
+
+  anson = Region.create(
+   :regionname => 'Anson', :postalcode => '07'
+)
+  tanjongPagar = Region.create(
+   :regionname => 'Tanjong Pagar', :postalcode => '08'
+)
+
+  anson = Region.create(
+   :regionname => 'Anson', :postalcode => '07'
+)
+  tanjongPagar = Region.create(
+   :regionname => 'Tanjong Pagar', :postalcode => '08'
+)
+  bukitmerah = Region.create(
+   :regionname => 'Bukit Merah', :postalcode => '14'
+)
+  queenstown = Region.create(
+   :regionname => 'Queenstown', :postalcode => '15'
+)
+
+  tiongBahru = Region.create(
+   :regionname => 'Tiong Bahru', :postalcode => '16'
+)
+
+  telokBlangah = Region.create(
+   :regionname => 'Telok Blangah', :postalcode => '09'
+)
+
+  harbourfront = Region.create(
+   :regionname => 'Harbourfront', :postalcode => '10'
+)
+
+  pasirPanjang = Region.create(
+   :regionname => 'Pasir Panjang', :postalcode => '11'
+)
+
+  hongLeong = Region.create(
+   :regionname => 'Hong Leong Garden', :postalcode => '12'
+)
+
+  clementiNewTown = Region.create(
+   :regionname => 'Clementi New Town', :postalcode => '13'
+)
+
+  highStreet = Region.create(
+   :regionname => 'High Street', :postalcode => '17'
+)
+
+  middleRoad = Region.create(
+   :regionname => 'Middle Road', :postalcode => '18'
+)
+
+  goldenMile = Region.create(
+   :regionname => 'Golden Mile', :postalcode => '19'
+)
+
+  littleIndia = Region.create(
+   :regionname => 'Little India', :postalcode => '20'
+)
+
+  lavender = Region.create(
+   :regionname => 'lavender', :postalcode => '21'
+)
+
+  orchard = Region.create(
+   :regionname => 'Orchard', :postalcode => '22'
+)
+
+  riverValley = Region.create(
+   :regionname => 'River Valley', :postalcode => '23'
+)
+
+  ardmore = Region.create(
+   :regionname => 'Ardmore', :postalcode => '24'
+)
+
+  bukitTimah = Region.create(
+   :regionname => 'Bukit Timah', :postalcode => '25'
+)
+
+  hollandRoad = Region.create(
+   :regionname => 'Holland Road', :postalcode => '26'
+)
+
+  tanglin = Region.create(
+   :regionname => 'Tanglin', :postalcode => '27'
+)
+
+  wattenEstate = Region.create(
+   :regionname => 'Watten Estate', :postalcode => '28'
+)
+
+  novena = Region.create(
+   :regionname => 'Novena', :postalcode => '29'
+)
+
+  thomson = Region.create(
+   :regionname => 'Thomson', :postalcode => '30'
+)
+
+  balestier = Region.create(
+   :regionname => 'Balestier', :postalcode => '31'
+)
+
+  toaPayoh = Region.create(
+   :regionname => 'Toa Payoh', :postalcode => '32'
+)
+
+  serangoon = Region.create(
+   :regionname => 'Serangoon', :postalcode => '33'
+)
+
+  macpherson1 = Region.create(
+   :regionname => 'Macpherson 1', :postalcode => '34'
+)
+
+  macpherson2 = Region.create(
+   :regionname => 'Macpherson 2', :postalcode => '35'
+)
+  bradell1 = Region.create(
+   :regionname => 'Bradell 1', :postalcode => '36'
+)
+
+    bradell2 = Region.create(
+   :regionname => 'Bradell 2', :postalcode => '37'
+)
+
+  geylang1 = Region.create(
+   :regionname => 'Geylang 1', :postalcode => '38'
+)
+
+  geylang2 = Region.create(
+   :regionname => 'Geylang 2', :postalcode => '39'
+)
+  eunos1 = Region.create(
+   :regionname => 'Eunos 1', :postalcode => '40'
+)
+
+  eunos2 = Region.create(
+   :regionname => 'Eunos 2', :postalcode => '41'
+)
 
-#   katong1 = Region.create(
-#    :regionname => 'Katong 1', :postalcode => '42'
-# )
+  katong1 = Region.create(
+   :regionname => 'Katong 1', :postalcode => '42'
+)
 
-#   katong2 = Region.create(
-#    :regionname => 'Katong 2', :postalcode => '43'
-# )
+  katong2 = Region.create(
+   :regionname => 'Katong 2', :postalcode => '43'
+)
 
-#   jooChiat = Region.create(
-#    :regionname => 'Joo Chiat', :postalcode => '44'
-# )
+  jooChiat = Region.create(
+   :regionname => 'Joo Chiat', :postalcode => '44'
+)
 
-#   amber = Region.create(
-#    :regionname => 'Amber Road', :postalcode => '45'
-# )
+  amber = Region.create(
+   :regionname => 'Amber Road', :postalcode => '45'
+)
 
-#   bedok = Region.create(
-#    :regionname => 'Bedok', :postalcode => '46'
-# )
+  bedok = Region.create(
+   :regionname => 'Bedok', :postalcode => '46'
+)
 
-#   eastwoord = Region.create(
-#    :regionname => 'East Woord', :postalcode => '47'
-# )
+  eastwoord = Region.create(
+   :regionname => 'East Woord', :postalcode => '47'
+)
 
-#   kewDrive = Region.create(
-#    :regionname => 'Kew Drive', :postalcode => '48'
-# )
+  kewDrive = Region.create(
+   :regionname => 'Kew Drive', :postalcode => '48'
+)
 
-#   loyang1 = Region.create(
-#    :regionname => 'Loyang 1', :postalcode => '49'
-# )
+  loyang1 = Region.create(
+   :regionname => 'Loyang 1', :postalcode => '49'
+)
 
-#   loyang2 = Region.create(
-#    :regionname => 'Loyang 2', :postalcode => '50'
-# )
+  loyang2 = Region.create(
+   :regionname => 'Loyang 2', :postalcode => '50'
+)
 
-#   changi = Region.create(
-#    :regionname => 'Changi', :postalcode => '81'
-# )
+  changi = Region.create(
+   :regionname => 'Changi', :postalcode => '81'
+)
 
-#   tampinese = Region.create(
-#    :regionname => 'Tampinese', :postalcode => '51'
-# )
+  tampinese = Region.create(
+   :regionname => 'Tampinese', :postalcode => '51'
+)
 
-#   pasiRis = Region.create(
-#    :regionname => 'Pasir Ris', :postalcode => '52'
-# )
+  pasiRis = Region.create(
+   :regionname => 'Pasir Ris', :postalcode => '52'
+)
 
-#   hougang1 = Region.create(
-#    :regionname => 'Serangoon Garden and hougang 1', :postalcode => '53'
-# )
+  hougang1 = Region.create(
+   :regionname => 'Serangoon Garden and hougang 1', :postalcode => '53'
+)
 
-#   kovan = Region.create(
-#    :regionname => 'kovan', :postalcode => '54'
-# )
+  kovan = Region.create(
+   :regionname => 'kovan', :postalcode => '54'
+)
 
-#   hougang2 = Region.create(
-#    :regionname => 'hougang2', :postalcode => '55'
-# )
+  hougang2 = Region.create(
+   :regionname => 'hougang2', :postalcode => '55'
+)
 
-#   punggol = Region.create(
-#    :regionname => 'Punggol', :postalcode => '82'
-# )
+  punggol = Region.create(
+   :regionname => 'Punggol', :postalcode => '82'
+)
 
-#   bishan = Region.create(
-#    :regionname => 'Bishan', :postalcode => '56'
-# )
+  bishan = Region.create(
+   :regionname => 'Bishan', :postalcode => '56'
+)
 
-#   angmokio = Region.create(
-#    :regionname => 'Ang Mo Kio', :postalcode => '57'
-# )
+  angmokio = Region.create(
+   :regionname => 'Ang Mo Kio', :postalcode => '57'
+)
 
-#   upperBukitTimah = Region.create(
-#    :regionname => 'Upper Bukit Timah', :postalcode => '58'
-# )
+  upperBukitTimah = Region.create(
+   :regionname => 'Upper Bukit Timah', :postalcode => '58'
+)
 
 
-#   uluPandan = Region.create(
-#    :regionname => 'Ulu Pandan', :postalcode => '59'
-# )
+  uluPandan = Region.create(
+   :regionname => 'Ulu Pandan', :postalcode => '59'
+)
 
-#   jurong1 = Region.create(
-#    :regionname => 'Jurong 1', :postalcode => '60'
-# )
+  jurong1 = Region.create(
+   :regionname => 'Jurong 1', :postalcode => '60'
+)
 
-#   jurong2 = Region.create(
-#    :regionname => 'Jurong 2', :postalcode => '61'
-# )
+  jurong2 = Region.create(
+   :regionname => 'Jurong 2', :postalcode => '61'
+)
 
-#   jurong3 = Region.create(
-#    :regionname => 'Jurong 3', :postalcode => '62'
-# )
+  jurong3 = Region.create(
+   :regionname => 'Jurong 3', :postalcode => '62'
+)
 
-#   jurong4 = Region.create(
-#    :regionname => 'Jurong 4', :postalcode => '63'
-# )
+  jurong4 = Region.create(
+   :regionname => 'Jurong 4', :postalcode => '63'
+)
 
 
 
-#   tuas = Region.create(
-#    :regionname => 'Tuas', :postalcode => '64'
-# )
+  tuas = Region.create(
+   :regionname => 'Tuas', :postalcode => '64'
+)
 
 
-#   hillview = Region.create(
-#    :regionname => 'Hillview', :postalcode => '65'
-# )
+  hillview = Region.create(
+   :regionname => 'Hillview', :postalcode => '65'
+)
 
-#   dairyFarm = Region.create(
-#    :regionname => 'Dairy Farm', :postalcode => '66'
-# )
+  dairyFarm = Region.create(
+   :regionname => 'Dairy Farm', :postalcode => '66'
+)
 
-#   bukitPanjang = Region.create(
-#    :regionname => 'Bukit Panjang', :postalcode => '67'
-# )
+  bukitPanjang = Region.create(
+   :regionname => 'Bukit Panjang', :postalcode => '67'
+)
 
-#   choaChuKang = Region.create(
-#    :regionname => 'Choa Chu Kang', :postalcode => '68'
-# )
+  choaChuKang = Region.create(
+   :regionname => 'Choa Chu Kang', :postalcode => '68'
+)
 
-#   limchukang1 = Region.create(
-#    :regionname => 'Lim Chu Kang 1', :postalcode => '69'
-# )
+  limchukang1 = Region.create(
+   :regionname => 'Lim Chu Kang 1', :postalcode => '69'
+)
 
-#   limchukang2 = Region.create(
-#    :regionname => 'Lim Chu Kang 2', :postalcode => '70'
-# )
+  limchukang2 = Region.create(
+   :regionname => 'Lim Chu Kang 2', :postalcode => '70'
+)
 
-#   tengah = Region.create(
-#    :regionname => 'Tengah', :postalcode => '71'
-# )
+  tengah = Region.create(
+   :regionname => 'Tengah', :postalcode => '71'
+)
 
-#   kranji = Region.create(
-#    :regionname => 'Kranji', :postalcode => '72'
-# )
+  kranji = Region.create(
+   :regionname => 'Kranji', :postalcode => '72'
+)
 
-#   woodlands = Region.create(
-#    :regionname => 'Woodlands and Woodgrobe', :postalcode => '73'
-# )
+  woodlands = Region.create(
+   :regionname => 'Woodlands and Woodgrobe', :postalcode => '73'
+)
 
-#   upperThomson = Region.create(
-#    :regionname => 'Upper Thomson', :postalcode => '77'
-# )
+  upperThomson = Region.create(
+   :regionname => 'Upper Thomson', :postalcode => '77'
+)
 
-#   springleaf = Region.create(
-#    :regionname => 'springLeaf', :postalcode => '78'
-# )
+  springleaf = Region.create(
+   :regionname => 'springLeaf', :postalcode => '78'
+)
 
-#   yishun = Region.create(
-#    :regionname => 'Yishun', :postalcode => '75'
-# )
+  yishun = Region.create(
+   :regionname => 'Yishun', :postalcode => '75'
+)
 
-#   sembawang = Region.create(
-#    :regionname => 'Sembawang', :postalcode => '76'
-# )
+  sembawang = Region.create(
+   :regionname => 'Sembawang', :postalcode => '76'
+)
 
-#   seletar1 = Region.create(
-#    :regionname => 'Seletar 1', :postalcode => '79'
-# )
+  seletar1 = Region.create(
+   :regionname => 'Seletar 1', :postalcode => '79'
+)
 
-#   seletar2 = Region.create(
-#    :regionname => 'Seletar 2', :postalcode => '80'
-# )
+  seletar2 = Region.create(
+   :regionname => 'Seletar 2', :postalcode => '80'
+)
 
 
 ascencio = Carpark.create(
